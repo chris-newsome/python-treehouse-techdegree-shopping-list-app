a845a7ee113caa80f750ab735e952c8f563cff2c
Chris <45610157+chris-newsome@users.noreply.github.com>
2019-07-08 23:27:11 -0400

    Update shopping_list.py

--- shopping_list.py
+++ shopping_list.py
@@ -16,12 +16,17 @@ def show_list():
 
 # Function to prompt the user about items in the cart
 def show_help():
+
     print("What should we pick up at the store?")
     print("""
-Enter 'DONE' to stop adding items.
-Enter 'HELP' for this help.
-Enter 'SHOW' to see your current list.
-""")
+
+    Enter 'DONE' to stop adding items.
+
+    Enter 'HELP' for this help.
+    
+    Enter 'SHOW' to see your current list.
+
+    """)
 
 show_help()
 
