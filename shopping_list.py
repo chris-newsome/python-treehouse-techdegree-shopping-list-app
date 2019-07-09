4edaed6643f92c20c45b14a7e3b2b7e95c718a4a
Chris <45610157+chris-newsome@users.noreply.github.com>
2019-07-08 23:38:49 -0400

    Update shopping_list.py

--- shopping_list.py
+++ shopping_list.py
@@ -3,6 +3,7 @@ shopping_list = []
 
 # Function that adds items to the list
 def add_to_list(item):
+
     shopping_list.append(item)
 
     # Notify the user that the item was added and state the number of items in the list
@@ -10,20 +11,24 @@ def add_to_list(item):
 
 # Function to show user the list in its current state
 def show_list():
+
     print("Here's your list:")
+
     for item in shopping_list:
+
         print(item)
 
 # Function to prompt the user about items in the cart
 def show_help():
 
     print("What should we pick up at the store?")
+
     print("""
 
     Enter 'DONE' to stop adding items.
 
     Enter 'HELP' for this help.
-    
+
     Enter 'SHOW' to see your current list.
 
     """)
@@ -32,16 +37,24 @@ show_help()
 
 # User is continually prompted to add item
 while True:
+
     new_item = input("> ")
 
     # When user is done, exit the loop
     if new_item == 'DONE':
+
         break
+
     elif new_item == 'HELP':
+
         show_help()
+
         continue
+
     elif new_item == 'SHOW':
+
         show_list()
+        
         continue
     
     # Call add_to_list with new_item as an argument
